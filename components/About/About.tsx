4bd2098cd072ea0d62899ca1bba972b9cff19799
mohamedseif <mohamed.hamza@groupado.com>
2021-12-08 10:15:17 +0100

    fix: about section

--- components/About/About.tsx
+++ components/About/About.tsx
@@ -7,15 +7,15 @@ const About = () => (
     <div className={styles.about}>
         <img src='developer.jpg' width='350' height='350' alt='me' />
         <div className={styles.about_description}>
-            <h2>Hey! i' m mohamed seifeddine Hamza</h2>
+            <h2>Hey ! i' m mohamed seifeddine Hamza</h2>
             <p>
                 I’m a JavaScript developer, currently working as a full time Frontend Developer,
                 my focus is always on building the best secure, reliable, and good User Experience websites.<br />I love learning new things and sharing my discoveries
                 with others through experiments and projects on Github.I'm currently exploring React and Node.
             </p>
-            <p>You can find me here</p>
+
             <div className={styles.about_description_social}>
-                <SocialLinks />
+                <p>You can find me here</p> <SocialLinks />
             </div>
 
 
